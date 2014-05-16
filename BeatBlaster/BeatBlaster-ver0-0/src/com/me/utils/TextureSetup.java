bdda111a936e683e47b9892da5851f48db890315
Samah3ll <amael.botton@uha.fr>
2014-05-16 17:12:50 +0200

    chemin absolu
    chemin absolu dans TextureSetup pour tout les pc.

--- BeatBlaster/BeatBlaster-ver0-0/src/com/me/utils/TextureSetup.java
+++ BeatBlaster/BeatBlaster-ver0-0/src/com/me/utils/TextureSetup.java
@@ -6,12 +6,13 @@ public class TextureSetup {
 	
 	public static void main(String[] args) {
 		//GameScreen textures
-        TexturePacker2.process("/Users/SamaHell/ENSISA/Java-workspace/BeatBlaster/BeatBlaster-ver0-0-desktop/bin/data/textures",
-        					   "/Users/SamaHell/ENSISA/Java-workspace/BeatBlaster/BeatBlaster-ver0-0-desktop/bin/data/textures",
+		final String path = System.getProperty("user.dir");
+        TexturePacker2.process(path + "-desktop/bin/data/textures",
+        					   path + "-desktop/bin/data/textures",
         						"textures.pack");
         //MenuScreen textures
-        TexturePacker2.process("/Users/SamaHell/ENSISA/Java-workspace/BeatBlaster/BeatBlaster-ver0-0-desktop/bin/Menu/textures",
-				   "/Users/SamaHell/ENSISA/Java-workspace/BeatBlaster/BeatBlaster-ver0-0-desktop/bin/Menu/textures",
+        TexturePacker2.process(path + "-desktop/bin/Menu/textures",
+				    			path + "-desktop/bin/Menu/textures",
 					"textures.pack");
         System.out.println("Done");
     }
